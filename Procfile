15774f281314b84797370cc7937e0f63fcef40da
Andrew Chubb <andrew.chubb@gmail.com>
2014-07-06 13:24:47 +0000

    Procfile Update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wagtaildemo.wsgi
+web: python manage.py runserver "0.0.0.0:$PORT" 
--- wagtaildemo/settings/base.py
+++ wagtaildemo/settings/base.py
@@ -4,7 +4,7 @@ import os
 
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -31,11 +31,11 @@ DATABASES = {
 
 if not os.environ.get("HOME") == '/home/andrew/Git/wagtaildemo':
     import dj_database_url
-    DATABASES['default'] = dj_database_url_config()
+    DATABASES['default'] = dj_database_url.config()
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
